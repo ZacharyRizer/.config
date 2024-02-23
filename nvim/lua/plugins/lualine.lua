a847d075eb57c328609e77d39e0cce78e603e132
ZacharyRizer <zacharyrizer@gmail.com>
2024-02-22 23:34:27 -0500

    global statusline update

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -12,7 +12,11 @@ return {
             options = {
                 component_separators = { left = '', right = '' },
                 section_separators = { left = '', right = '' },
-                disabled_filetypes = { "dashboard", "NvimTree", "undotree" }
+                refresh = {
+                    statusline = 100,
+                    tabline = 100,
+                    winbar = 100,
+                }
             },
             sections = {
                 lualine_a = { { "mode", fmt = function(str) return str:sub(1, 1) end, } },
@@ -34,6 +38,8 @@ return {
                 lualine_y = { { "progress", cond = big_screen } },
                 lualine_z = { { "location", cond = big_screen } },
             },
+            winbar = { lualine_y = { 'filename' } },
+            inactive_winbar = { lualine_y = { 'filename' } }
         })
     end,
 }
