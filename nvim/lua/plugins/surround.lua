f5c263b8a0fff6e7171faaeaa5f05789141c8e49
ZacharyRizer <zacharyrizer@gmail.com>
2024-06-24 16:21:16 -0400

    Update neovim

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -2,8 +2,6 @@ return {
 	"kylechui/nvim-surround",
 	event = "VeryLazy",
 	config = function()
-		require("nvim-surround").setup({
-			keymaps = { visual = "<Leader>s" },
-		})
+		require("nvim-surround").setup()
 	end,
 }
