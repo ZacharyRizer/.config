bc3e3be92f33524a161a21ee94ed2b4570edc5f2
ZacharyRizer <zacharyrizer@gmail.com>
2025-03-06 12:22:36 -0500

    Small updates to grugfar

--- nvim/lua/plugins/grug-far.lua
+++ nvim/lua/plugins/grug-far.lua
@@ -2,7 +2,13 @@ return {
 	"MagicDuck/grug-far.nvim",
 	keys = { "<Leader>G" },
 	config = function()
-		require("grug-far").setup({})
-		V.map("n", "<Leader>G", '<cmd>lua require("grug-far").open()<CR>')
+		require("grug-far").setup({
+			engines = {
+				ripgrep = {
+					extraArgs = "-S",
+				},
+			},
+		})
+		V.map({ "n", "v" }, "<Leader>G", '<cmd>lua require("grug-far").open()<CR>')
 	end,
 }
