4eedf61cf45ab8313ea1a1ab205dd4c7c255b54d
ZacharyRizer <zacharyrizer@gmail.com>
2025-11-17 11:27:48 -0500

    add symlink setup file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ~/.config/fzf/.fzf.zsh ~/.fzf.zsh
+ln -s ~/.config/git/.gitconfig ~/.gitconfig
+ln -s ~/.config/intellij/.ideavimrc ~/.ideavimrc
+ln -s ~/.config/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.config/zsh/.zshrc ~/.zshrc
+ln -s ~/.config/hammerspoon/init.lua ~/.hammerspoon/init.lua
